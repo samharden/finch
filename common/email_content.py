b6d5103793fb474de4de3cd32a1993af513ed7be
samharden <samharden@gmail.com>
2018-06-12 00:39:47 -0400

    Why

--- common/email_content.py
+++ common/email_content.py
@@ -110,8 +110,8 @@ email_body = """
 
 
 
-                        <a href="%s" target="_blank" style="display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;">
-                          View your post!</a> </td><br></br>
+                        <a href="" target="_blank" style="display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;">
+                          View your post! %s </a> </td><br></br>
                           <p>
                             When someone responds to your post, we'll alert you
                                 via email.</p>
