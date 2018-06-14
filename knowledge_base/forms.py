a5ea0162c08d2c0ba95f3e0499be1a7a4b7e5c7a
samharden <samharden@gmail.com>
2018-06-14 08:42:19 -0400

    ok

--- knowledge_base/forms.py
+++ knowledge_base/forms.py
@@ -40,6 +40,7 @@ class KnowledgebaseForm(forms.ModelForm):
         self.fields['neg_resp'].required = False
         self.fields['body'].required = False
         self.fields['plain_body'].required = False
+        self.fields['kb_type'].required = False
 
         issue_detail = forms.CharField(widget=forms.Textarea)
 
